c0a91883c227464cdf7561ddfb6772c50511835a
Tarun-8042 <68146896+Tarun-8042@users.noreply.github.com>
2023-04-10 10:47:47 +0530

    Add files via upload

---
+++ GUESS NUM VS RANDOMN/guessing num vs random um.js
@@ -0,0 +1,15 @@
+let gameResult = document.getElementById("gameResult");
+let userInput = document.getElementById("userInput");
+let randomNumber = Math.ceil(Math.random() * 10);
+console.log(randomNumber)
+
+function checkGuess() {
+    let guessNumber = parseInt(userInput.value);
+    if (guessNumber > randomNumber) {
+        gameResult.textContent = "Too High,Try Again!";
+        gameResult.style.backgroundColor = "#1e217c"
+    } else if (guessNumber < randomNumber) {
+        gameResult.textContent = "Too Low,Try Again!";
+        gameResult.style.backgroundColor = "#1e217c"
+    }
+}
